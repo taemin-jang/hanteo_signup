29e2729a67d1e11476d8f8e75b7cb1cddc5ef8ce
taemin-Jang <jtm990506@gmail.com>
2024-04-11 17:01:59 +0900

    Fix: render error 수정

--- src/pages/FallbackComponent.tsx
+++ src/pages/FallbackComponent.tsx
@@ -1,14 +1,7 @@
 import { FallbackProps } from 'react-error-boundary';
 import Button from '../components/Button';
-import useResolution from '../hooks/useResolution';
-import { useEffect } from 'react';
 
 const FallbackComponent = ({ error, resetErrorBoundary }: FallbackProps) => {
-  const resolutionError = useResolution();
-  useEffect(() => {
-    // 지원되는 해상도로 변경 시 Error 초기화
-    if (!resolutionError) resetErrorBoundary();
-  });
   return (
     <div>
       <h2>{error.message}</h2>
